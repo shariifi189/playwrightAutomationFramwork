b21c476526b69d99d2847ec89ccfa0ceffce0f34
shariif189 <shariifi189@gmail.com>
2026-01-15 14:35:31 -0500

    uplaod file completed and fixture dir added

--- tests/uploadFile.spec.js
+++ tests/uploadFile.spec.js
@@ -1,2 +1,21 @@
+import {test, expect } from '@playwright/test';
 
-// we need a tc for upload file
+test.describe('File Upload Test ', () => {
+    test('Should upload a file seccessfully', async ({ page }) => {
+
+        await page.goto("https://kitchen.applitools.com/")
+       //wait expect(page.getByRole('link', { name: "The Kitche"})).toBeVisible();
+
+        await page.getByRole('link', { name: 'File Picker' }).click();
+  
+      //await page.getByRole('button', { name: "Upload Recipe Picture" }).click();
+        await page.setInputFiles(
+            'input[type="file"]',
+            'tests/fixtures/jenkins.png'
+        );
+
+        await expect(page.locator('input[type="file"]'))
+    .toHaveValue(/jenkins.png/);
+        await page.pause
+    });
+});
